62fd856b815e40548084c2f9d7251546cce6ad9f
Shawn Rutledge <s@ecloud.org>
2020-12-12 22:27:02 +0100

    take json from stdin and create a DAG node
    curl http://10.42.101.249/j | jq .data | cargo run -- insert

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,24 @@
 **
 ****************************************************************************/
 
+use clap::{App, Arg};
 use futures::TryStreamExt;
 use ipfs_api::IpfsClient;
-use serde_json::Value;
+use serde_json;
 use std::any::type_name;
+use std::io;
 use std::io::Cursor;
 use tokio;
 
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
-
 #[tokio::main]
-async fn main() {
+async fn insert_from_json<R: io::Read>(rdr: R) {
+    let json_data: serde_json::Value = serde_json::from_reader(rdr).unwrap();
+    println!("deserialized input = {:?}", json_data);
+
+    println!("reconstructed input {}", json_data.to_string());
+
     let client = IpfsClient::default();
-    let data = r#"{"hello":423453343}"#;
-    let dag_node = Cursor::new(data);
-    println!("{}", type_of(data));
+    let dag_node = io::Cursor::new(json_data.to_string());
 
     let response = client
         .dag_put(dag_node)
@@ -49,11 +50,57 @@ async fn main() {
     {
         Ok(bytes) => {
             println!("{}", String::from_utf8_lossy(&bytes[..]));
-            let deserialized: Value = serde_json::from_slice(&bytes).unwrap();
-            println!("deserialized = {:?}", deserialized);
+            let deserialized: serde_json::Value = serde_json::from_slice(&bytes).unwrap();
+            println!("deserialized dag node = {:?}", deserialized);
         }
         Err(e) => {
             eprintln!("error reading dag node: {}", e);
         }
+    }
+}
+
+fn main() {
+    let matches = App::new("iptsdb")
+        .version("0.1")
+        .about("client for an IPFS-based time-series database")
+        .arg(
+            Arg::with_name("format")
+                .short("f")
+                .long("format")
+                .value_name("fmt")
+                .help("Sets the input or output format")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("file")
+                .help("Sets the file to use (default is stdin/stdout)")
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("query")
+                .help("insert or select")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .get_matches();
+
+    let fmt = matches.value_of("format").unwrap_or("json");
+    println!("Value for fmt: {}", fmt);
+
+    if let Some(matches) = matches.value_of("file") {
+        println!("Using file: {}", matches);
+    }
+
+    let _verbosity = matches.occurrences_of("v");
+
+    match matches.value_of("query").unwrap() {
+        "insert" => insert_from_json(io::stdin()),
+        _ => println!("only insert is supported for now"),
     }
 }
