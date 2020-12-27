90d5f56ea3dde21a000aff3e2173908ee1a4f404
Shawn Rutledge <s@ecloud.org>
2020-12-28 00:08:58 +0100

    extract resolve_root() and get_node_json(); env_logger rather than -v
    set -x RUST_LOG ipfs_tsdb_rust=debug,info
    should be roughly equivalent to -v -v
    
    The code is more compact with less nested error checking now.
    
    However I discovered that I can't upgrade tokio to the current
    1.0.1 release, or even 0.3:
    
    thread 'main' panicked at 'there is no reactor running, must be called from the
    context of Tokio runtime',
    .cargo/registry/src/github.com-1ecc6299db9ec823/tokio-0.2.24/src/io/driver/mod.rs:204:14
    
    question is how to call an async fn from a tokio::main fn; otherwise
    it seems like functional decomposition is more difficult than it should be.

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,10 @@
 extern crate prettytable;
 use chrono::prelude::*;
 use clap::{App, Arg, SubCommand};
+use env_logger;
 use futures::TryStreamExt;
 use ipfs_api::IpfsClient;
+use log::{debug, info, trace};
 use prettytable::{format, Row, Table};
 use serde_json;
 use std::collections::HashMap;
@@ -31,131 +33,120 @@ use tokio::time::timeout;
 
 type JsonMap = HashMap<String, serde_json::Value>;
 
-static mut VERBOSITY: u64 = 0;
 const PUBLISH_TIMEOUT: u64 = 50;
 const TIMESTAMP_KEY: &str = "_timestamp";
 
+async fn resolve_root(
+	client: &IpfsClient,
+	ipnskey: &str,
+) -> Result<String, ipfs_api::response::Error> {
+	let key_list = client.key_list().await?;
+	let ipns_name = &key_list
+		.keys
+		.iter()
+		.find(|&keypair| keypair.name == ipnskey)
+		.unwrap()
+		.id[..];
+	match client.name_resolve(Some(ipns_name), true, false).await {
+		Ok(resolved) => {
+			debug!("resolved {}", ipns_name);
+			Ok(resolved.path.to_string())
+		}
+		Err(e) => Err(e),
+	}
+}
+
+async fn get_node_json(
+	client: &IpfsClient,
+	path: &str,
+) -> Result<JsonMap, ipfs_api::response::Error> {
+	match client
+		.dag_get(&path)
+		.map_ok(|chunk| chunk.to_vec())
+		.try_concat()
+		.await
+	{
+		Ok(bytes) => {
+			debug!("dag_get {} done", &path);
+			Ok(serde_json::from_slice(&bytes).unwrap())
+		}
+		Err(e) => Err(e),
+	}
+}
+
 #[tokio::main]
 async fn select_json(ipnskey: &str, limit: i32, query: Vec<&str>) {
-	let begintime = SystemTime::now();
-	let verbosity = unsafe { VERBOSITY };
-	if verbosity > 0 {
-		eprintln!(
-			"select from {} limit {:?} fields {:?}",
-			ipnskey, limit, query
-		);
-	}
+	debug!(
+		"select from {} limit {:?} fields {:?}",
+		ipnskey, limit, query
+	);
 
 	let client = IpfsClient::default();
-
-	match client.key_list().await {
-		Ok(list) => {
-			let ipns_name = &list
-				.keys
-				.iter()
-				.find(|&keypair| keypair.name == ipnskey)
-				.unwrap()
-				.id[..];
-			match client.name_resolve(Some(ipns_name), true, false).await {
-				Ok(resolved) => {
-					if verbosity > 0 {
-						eprintln!(
-							"resolved {} after {} ms",
-							ipns_name,
-							begintime.elapsed().unwrap().as_millis()
-						);
-					}
-					match client
-						.dag_get(&resolved.path)
-						.map_ok(|chunk| chunk.to_vec())
-						.try_concat()
-						.await
-					{
-						Ok(bytes) => {
-							if verbosity > 0 {
-								eprintln!(
-									"dag_get {} done @ {} ms",
-									&resolved.path,
-									begintime.elapsed().unwrap().as_millis()
-								);
-							}
-							// TODO if "select *" (empty query?) replace it with
-							// for (key, value) in all_records.iter_mut() { ...
-							let all_records: JsonMap = serde_json::from_slice(&bytes).unwrap();
-							let mut row_count: usize = 0; // = all_records.values() .len();
-							let mut record_values = Vec::new(); //: Vec<serde_json::Value>;
-							let mut title_row = Row::empty();
-							for field_key in &query {
-								if field_key == &TIMESTAMP_KEY {
-									title_row.add_cell(cell!("timestamp (UTC)"));
-								} else {
-									title_row.add_cell(cell!(field_key));
-								}
-								let values = all_records
-									.get(*field_key)
-									.unwrap()
-									.as_array()
-									.unwrap()
-									.to_vec();
-								if row_count == 0 {
-									row_count = values.len();
-								}
-								assert_eq!(row_count, values.len()); // values were missing during some inserts?
-								if limit > -1 {
-									record_values.push(
-										values[(row_count - (limit as usize))..row_count].to_vec(),
-									);
-								} else {
-									record_values.push(values);
-								}
-								//~ println!("{}: {:?} records", field_key, row_count);
-								//~ println!("{}: {:?}", field_key, record_values[record_values.len()]);
-							}
-							let mut table = Table::new();
-							let format = format::FormatBuilder::new()
-								.column_separator('|')
-								.borders('|')
-								.separators(
-									&[format::LinePosition::Title],
-									format::LineSeparator::new('-', '|', '|', '|'),
-								)
-								.padding(1, 1)
-								.build();
-							table.set_format(format);
-							table.set_titles(title_row);
-							if limit > -1 {
-								row_count = limit as usize;
-							}
-							for r in 0..row_count {
-								let mut row = Row::empty();
-								for c in 0..record_values.len() {
-									if query[c] == TIMESTAMP_KEY {
-										let ts = record_values[c][r].as_i64().unwrap();
-										let dt: chrono::DateTime<Utc> = DateTime::from_utc(
-											NaiveDateTime::from_timestamp(ts, 0),
-											Utc,
-										);
-										row.add_cell(cell!(dt.format("%Y-%m-%d %H:%M")));
-									} else {
-										row.add_cell(cell!(record_values[c][r].to_string()));
-									}
-								}
-								table.add_row(row);
-							}
-							table.printstd();
-						}
-						Err(e) => {
-							eprintln!("error reading dag node: {}", e);
-						}
-					}
+	let resolved_path = resolve_root(&client, &ipnskey).await.unwrap();
+	match get_node_json(&client, &resolved_path).await {
+		Ok(all_records) => {
+			// TODO if "select *" (empty query?) replace it with
+			// for (key, value) in all_records.iter_mut() { ...
+			let mut row_count: usize = 0; // = all_records.values() .len();
+			let mut record_values = Vec::new(); //: Vec<serde_json::Value>;
+			let mut title_row = Row::empty();
+			for field_key in &query {
+				if field_key == &TIMESTAMP_KEY {
+					title_row.add_cell(cell!("timestamp (UTC)"));
+				} else {
+					title_row.add_cell(cell!(field_key));
 				}
-				Err(e) => {
-					eprintln!("error resolving {}: {}", ipnskey, e);
+				let values = all_records
+					.get(*field_key)
+					.unwrap()
+					.as_array()
+					.unwrap()
+					.to_vec();
+				if row_count == 0 {
+					row_count = values.len();
 				}
+				assert_eq!(row_count, values.len()); // values were missing during some inserts?
+				if limit > -1 {
+					record_values.push(values[(row_count - (limit as usize))..row_count].to_vec());
+				} else {
+					record_values.push(values);
+				}
+				//~ println!("{}: {:?} records", field_key, row_count);
+				//~ println!("{}: {:?}", field_key, record_values[record_values.len()]);
 			}
+			let mut table = Table::new();
+			let format = format::FormatBuilder::new()
+				.column_separator('|')
+				.borders('|')
+				.separators(
+					&[format::LinePosition::Title],
+					format::LineSeparator::new('-', '|', '|', '|'),
+				)
+				.padding(1, 1)
+				.build();
+			table.set_format(format);
+			table.set_titles(title_row);
+			if limit > -1 {
+				row_count = limit as usize;
+			}
+			for r in 0..row_count {
+				let mut row = Row::empty();
+				for c in 0..record_values.len() {
+					if query[c] == TIMESTAMP_KEY {
+						let ts = record_values[c][r].as_i64().unwrap();
+						let dt: chrono::DateTime<Utc> =
+							DateTime::from_utc(NaiveDateTime::from_timestamp(ts, 0), Utc);
+						row.add_cell(cell!(dt.format("%Y-%m-%d %H:%M")));
+					} else {
+						row.add_cell(cell!(record_values[c][r].to_string()));
+					}
+				}
+				table.add_row(row);
+			}
+			table.printstd();
 		}
 		Err(e) => {
-			eprintln!("error getting keys: {}", e);
+			eprintln!("error reading dag node: {}", e);
 		}
 	}
 }
@@ -163,7 +154,6 @@ async fn select_json(ipnskey: &str, limit: i32, query: Vec<&str>) {
 #[tokio::main]
 async fn insert_from_json<R: io::Read>(ipnskey: &str, rdr: R) -> String {
 	let begintime = SystemTime::now();
-	let verbosity = unsafe { VERBOSITY };
 	let unixtime = serde_json::json!(
 		match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
 			Ok(n) => n.as_secs(),
@@ -172,133 +162,90 @@ async fn insert_from_json<R: io::Read>(ipnskey: &str, rdr: R) -> String {
 	);
 
 	let json_data: JsonMap = serde_json::from_reader(rdr).unwrap();
-	if verbosity > 1 {
-		println!("given {:?}", json_data);
-	}
+	debug!("given {:?}", json_data);
 
 	let client = IpfsClient::default();
 
-	match client.key_list().await {
-		Ok(list) => {
-			let ipns_name = &list
-				.keys
-				.iter()
-				.find(|&keypair| keypair.name == ipnskey)
-				.unwrap()
-				.id[..];
-			match client.name_resolve(Some(ipns_name), true, false).await {
-				Ok(resolved) => {
-					if verbosity > 1 {
-						println!(
-							"resolved {} after {} ms",
-							ipns_name,
-							begintime.elapsed().unwrap().as_millis()
-						);
+	match resolve_root(&client, &ipnskey).await {
+		Ok(resolved_path) => {
+			match client
+				.dag_get(&resolved_path)
+				.map_ok(|chunk| chunk.to_vec())
+				.try_concat()
+				.await
+			{
+				Ok(bytes) => {
+					let mut existing: JsonMap = serde_json::from_slice(&bytes).unwrap();
+					for (key, value) in existing.iter_mut() {
+						let new_value: &serde_json::Value = match key.as_str() {
+							TIMESTAMP_KEY => &unixtime,
+							_ => json_data.get(key).unwrap(),
+						};
+						trace!("{:?} {:?} <- {:?}", key, value, new_value);
+						let vec = value.as_array_mut().unwrap();
+						vec.push(new_value.clone());
 					}
-					match client
-						.dag_get(&resolved.path)
-						.map_ok(|chunk| chunk.to_vec())
-						.try_concat()
-						.await
-					{
-						Ok(bytes) => {
-							if verbosity > 1 {
-								println!(
-									"dag_get {} done @ {} ms",
-									&resolved.path,
-									begintime.elapsed().unwrap().as_millis()
-								);
-							}
-							let mut existing: JsonMap = serde_json::from_slice(&bytes).unwrap();
-							for (key, value) in existing.iter_mut() {
-								let new_value: &serde_json::Value = match key.as_str() {
-									TIMESTAMP_KEY => &unixtime,
-									_ => json_data.get(key).unwrap(),
-								};
-								if verbosity > 2 {
-									println!("{:?} {:?} <- {:?}", key, value, new_value);
-								}
-								let vec = value.as_array_mut().unwrap();
-								vec.push(new_value.clone());
-							}
-							let cursor = io::Cursor::new(serde_json::json!(existing).to_string());
-							let response = client.dag_put(cursor).await.expect("dag_put error");
-							let cid = response.cid.cid_string;
-							if verbosity > 1 {
-								println!(
-									"ipns {} {} {} -> {} @ {} ms",
-									ipnskey,
-									ipns_name,
-									&resolved.path,
-									cid,
-									begintime.elapsed().unwrap().as_millis()
-								);
-							}
-							client.pin_add(&cid, false).await.expect("pin error");
-							if verbosity > 1 {
-								println!(
-									"pinned @ {} ms",
-									begintime.elapsed().unwrap().as_millis()
-								);
-							}
-							let fut =
-								client.name_publish(&cid, false, Some("12h"), None, Some(ipnskey));
-							match timeout(Duration::from_secs(PUBLISH_TIMEOUT), fut).await {
-								Ok(_res) => {
-									if verbosity > 0 {
-										println!(
-											"published {} @ {} ms",
-											&cid,
-											begintime.elapsed().unwrap().as_millis()
-										);
-									}
-									let _ = client.pin_rm(&resolved.path, false).await;
-									if verbosity > 1 {
-										println!(
-											"unpinned old @ {} ms",
-											begintime.elapsed().unwrap().as_millis()
-										);
-									}
-									client
-										.block_rm(&resolved.path)
-										.await
-										.expect("error removing last");
-									if verbosity > 1 {
-										println!(
-											"block_rm(old) done @ {} ms",
-											begintime.elapsed().unwrap().as_millis()
-										);
-									}
-								}
-								Err(e) => println!(
-									"error or timeout while publishing {} @ {} ms: {:?}",
-									&cid,
-									begintime.elapsed().unwrap().as_millis(),
-									e
-								),
-							}
-							return cid;
+					let cursor = io::Cursor::new(serde_json::json!(existing).to_string());
+					let response = client.dag_put(cursor).await.expect("dag_put error");
+					let cid = response.cid.cid_string;
+					debug!(
+						"ipns {} {} -> {} @ {} ms",
+						ipnskey,
+						&resolved_path,
+						cid,
+						begintime.elapsed().unwrap().as_millis()
+					);
+					client.pin_add(&cid, false).await.expect("pin error");
+					debug!("pinned @ {} ms", begintime.elapsed().unwrap().as_millis());
+					let fut = client.name_publish(&cid, false, Some("12h"), None, Some(ipnskey));
+					match timeout(Duration::from_secs(PUBLISH_TIMEOUT), fut).await {
+						Ok(_res) => {
+							// [2020-12-27T23:21:33Z INFO  ipfs_tsdb_rust] published bafyreic6cqei2aoxmfv47lioibm3dpqiqwv7ahuvxxa5ww4r2oul6ron4y @ 240776 ms
+							// should have timed out
+							info!(
+								"published {} @ {} ms",
+								&cid,
+								begintime.elapsed().unwrap().as_millis()
+							);
+							let _ = client.pin_rm(&resolved_path, false).await;
+							debug!(
+								"unpinned old @ {} ms",
+								begintime.elapsed().unwrap().as_millis()
+							);
+							client
+								.block_rm(&resolved_path)
+								.await
+								.expect("error removing last");
+							debug!(
+								"block_rm(old) done @ {} ms",
+								begintime.elapsed().unwrap().as_millis()
+							);
 						}
-						Err(e) => {
-							eprintln!("error reading dag node: {}", e);
-							return "".to_string();
-						}
+						Err(e) => eprintln!(
+							"error or timeout while publishing {} @ {} ms: {:?}",
+							&cid,
+							begintime.elapsed().unwrap().as_millis(),
+							e
+						),
 					}
+					return cid;
 				}
 				Err(e) => {
-					eprintln!("error resolving {}: {}", ipnskey, e);
+					eprintln!("error reading dag node: {}", e);
 					return "".to_string();
 				}
 			}
 		}
 		Err(e) => {
-			eprintln!("error getting keys: {}", e);
+			eprintln!("error resolving {}: {}", ipnskey, e);
 			return "".to_string();
 		}
 	}
 }
 
 fn main() {
+	env_logger::init();
+
 	let matches = App::new("iptsdb")
 		.version("0.1")
 		.about("client for an IPFS-based time-series database")
@@ -315,12 +262,6 @@ fn main() {
 				.value_name("fmt")
 				.help("Sets the input or output format")
 				.takes_value(true),
-		)
-		.arg(
-			Arg::with_name("v")
-				.short("v")
-				.multiple(true)
-				.help("Sets the level of verbosity"),
 		)
 		.arg(
 			Arg::with_name("input")
@@ -365,10 +306,6 @@ fn main() {
 	}
 	if let Some(matches) = matches.value_of("output") {
 		println!("output file: {} (not supported yet)", matches);
-	}
-
-	unsafe {
-		VERBOSITY = matches.occurrences_of("v");
 	}
 
 	let key = matches.value_of("ipnskey").unwrap();
